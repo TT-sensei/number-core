3d86a03203d243077eca31ba753400ba32e4314c
TT-sensei <rlrieaeoy@gmail.com>
2026-09-06 00:00:33 +0900

    Reset number-core: remove old project files

--- monster-ability-audit.js
+++
@@ -1,4 +0,0 @@
-// Compatibility layer: monster abilities are handled centrally in gameplay-fix.js.
-(() => {
-  if (typeof applyPlayerCommand === 'function') window.applyPlayerCommand = applyPlayerCommand;
-})();
